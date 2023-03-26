0e024f03ed4f634ee6235d8025b6caf54f606424
Dominic Ruggiero <dominic@userexe.me>
2023-03-26 18:26:30 -0400

    fix

--- setup.js
+++ setup.js
@@ -1,5 +1,5 @@
-import { REST } from '@discordjs/rest';
-import { Routes } from 'discord-api-types/v10';
+const { REST } = require('@discordjs/rest')
+const { Routes } = require('discord-api-types/v10');
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 

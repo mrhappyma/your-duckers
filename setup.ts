236c06c6527e1c837ac0f1deec35db9d3db148a0
Dominic Ruggiero <dominic@userexe.me>
2024-03-19 12:58:52 +0000

    add user install support

--- setup.ts
+++ setup.ts
@@ -1,21 +1,20 @@
 import environmentVariables from "./utils/env";
 import { REST } from "@discordjs/rest";
 import {
-  type RESTPostAPIApplicationCommandsJSONBody,
   Routes,
 } from "discord-api-types/v10";
 
 const env = environmentVariables.parse(process.env);
 const rest = new REST({ version: "10" }).setToken(env.TOKEN);
 
-const commands: RESTPostAPIApplicationCommandsJSONBody[] = [
+const commands = [
   {
     name: "quack",
     type: 1,
     description: "get a duck :)",
+    integration_types: [0,1]
   },
   {
-    dm_permission: false,
     name: "dotd",
     type: 1,
     description: "Set up the Duck of The Day",
@@ -26,14 +25,17 @@ const commands: RESTPostAPIApplicationCommandsJSONBody[] = [
         description: "Role to mention when the DOTD is posted",
       },
     ],
+    integration_types: [0],
+    contexts: [0]
   },
   {
     name: "info",
     type: 1,
     description: "Learn about Duckers",
+    integration_types: [0,1]
   },
 ];
 
 rest.put(Routes.applicationCommands(env.APPLICATION_ID), {
   body: commands,
-});
+}).then(r => console.log(r))

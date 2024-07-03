f9eec3fd2e38a587d975a4c24180a927c867ee2b
Dominic Ruggiero <dominic@userexe.me>
2024-07-03 17:30:35 -0400

    ducks are broke :(

--- commands/quack.ts
+++ commands/quack.ts
@@ -8,23 +8,50 @@ const quackCommand = async (message: any, response: VercelResponse) => {
     event: "duck-sent",
   });
   let duckData = {};
-  await fetch("https://random-d.uk/api/v2/random")
-    .then((response) => response.json())
-    .then((data) => (duckData = data));
-  const theDuck = duckResponse.parse(duckData);
-  response.status(200).send({
+  // await fetch("https://random-d.uk/api/v2/random")
+  //   .then((response) => response.json())
+  //   .then((data) => (duckData = data));
+//   const theDuck = duckResponse.parse(duckData);
+//   response.status(200).send({
+//     type: 4,
+//     data: {
+//       embeds: [
+//         {
+//           image: {
+//             url: theDuck.url,
+//           },
+//           color: 16766977,
+//         },
+//       ],
+//     },
+//   });
+   response.status(200).send({
     type: 4,
     data: {
       embeds: [
         {
-          image: {
-            url: theDuck.url,
-          },
+          title: "ducks are temporarially unavailable",
+          description:
+            "We're working on it, sorry! Join the Discord to be notified when ducks are back.",
           color: 16766977,
+        },
+      ],
+      components: [
+        {
+          type: 1,
+          components: [
+            {
+              type: 2,
+              label: "Duckers Support server",
+              style: 5,
+              url: "https://discord.gg/XSS5h53mXg",
+            },
+          ],
         },
       ],
     },
   });
+};
 };
 
 export default quackCommand;
